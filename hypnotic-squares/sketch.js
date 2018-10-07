2e1a5915595b0be6625af4bca4869b0f68cb5cea
Renato Torres <renato.torres@unit-consulting.ae>
2018-10-07 08:53:29 +0100

    Final adjustments

--- hypnotic-squares/sketch.js
+++ hypnotic-squares/sketch.js
@@ -7,7 +7,7 @@ let h = size * dpr;
 let finalSize = 3;
 let startSteps;
 let offset = 200;
-let tileStep = (size - offset * 2) / 50;
+let tileStep = (size - offset * 2) / 25;
 let startSize = tileStep;
 let directions = [-1, 0, 1];
 
@@ -23,21 +23,20 @@ function draw() {
 	stroke(360, 100, 0);
 	rect(50, 50, size - 100, size - 100);
 
-
+	// Draws the content
 	strokeWeight(0.5);
 	for (var x = offset; x < size - offset; x += tileStep) {
 		for (var y = offset; y < size - offset; y += tileStep) {
 			stroke(random(0, 360), 100, 100);
-
 			startSteps = 2 + Math.ceil(Math.random() * 3);
 			var xDirection = directions[Math.floor(Math.random() * directions.length)];
 			var yDirection = directions[Math.floor(Math.random() * directions.length)];
-			doit(x, y, startSize, startSize, xDirection, yDirection, startSteps - 1);
+			drawRecursive(x, y, startSize, startSize, xDirection, yDirection, startSteps - 1);
 		}
 	}
 }
 
-function doit(x, y, width, height, xMovement, yMovement, steps) {
+function drawRecursive(x, y, width, height, xMovement, yMovement, steps) {
 	rect(x, y, width, height);
 	if (steps >= 0) {
 		var newSize = (startSize) * (steps / startSteps) + finalSize;
@@ -45,6 +44,6 @@ function doit(x, y, width, height, xMovement, yMovement, steps) {
 		var newY = y + (height - newSize) / 2;
 		newX = newX - ((x - newX) / (steps + 2)) * xMovement;
 		newY = newY - ((y - newY) / (steps + 2)) * yMovement;
-		doit(newX, newY, newSize, newSize, xMovement, yMovement, steps - 1);
+		drawRecursive(newX, newY, newSize, newSize, xMovement, yMovement, steps - 1);
 	}
 }
